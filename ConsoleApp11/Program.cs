f07c1818c1d29fe4c4bbd77508cd481a3a494d70
mindedge26 <mindedge26@MEDGE164>
2022-05-26 11:21:41 +0530

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+   public class ArrayExample
+    {
+        static void PrintMin(int[] arr)
+        {
+            int min = arr[0];
+            for (int i=1; i<arr.Length; i++)
+            {
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine("Minimum Lenth:" + min);
+        }
+        public static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 20, 30, 40, 50, };
+            int[] arr2 = { 2, 39, 40, 38, 28, 59 };
+            PrintMin(arr1);
+            PrintMin(arr2);
+         }
+    }
+}
